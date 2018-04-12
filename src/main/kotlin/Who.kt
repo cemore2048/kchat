e5daf90ed8d1cc14cf79f211d81da2bab96c98cd
Rafa Moreno Cesar <rmoreno.cesar@gmail.com>
2018-04-11 22:28:18 -0500

    build.gradle is working, and changed things inside of the main so that it lines up with the basic tutorial

--- src/main/kotlin/Who.kt
+++ src/main/kotlin/Who.kt
@@ -1,18 +1,25 @@
 import io.ktor.application.*
 import io.ktor.features.CallLogging
 import io.ktor.features.DefaultHeaders
+import io.ktor.http.ContentType
 import io.ktor.response.respondText
-import io.ktor.routing.Routing
 import io.ktor.routing.get
+import io.ktor.routing.routing
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
 
+fun main(args: Array<String>) {
+    val server = embeddedServer(Netty, 8080) {
+        routing {
+            get("/") {
+                call.respondText("Hello world", ContentType.Text.Plain)
+            }
 
-fun Application.main() {
-    install(DefaultHeaders)
-    install(CallLogging)
-    install(Routing) {
-        get("/") {
-            val text = "Howdy, Planet!"
-            call.respondText(text)
+            get("/demo") {
+                call.respondText {"Hello Worldd"}
+            }
         }
     }
+    server.start(wait = true)
 }
+
